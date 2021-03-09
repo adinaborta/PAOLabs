cad1e54eed95043f8f43316f6c1778f1a2d0843f
adinaborta <62395363+adinaborta@users.noreply.github.com>
2021-03-09 10:17:29 +0200

    Lab 2 (dar cu acolade)

--- Laborator2/src/com/company/problema4/Singleton.java
+++ Laborator2/src/com/company/problema4/Singleton.java
@@ -9,8 +9,9 @@ class Singleton {
     }
 
     public static Singleton getSingleton(){
-        if(singleton == null)
+        if(singleton == null){
             singleton = new Singleton();
+        }
         return singleton;
     }
 }
